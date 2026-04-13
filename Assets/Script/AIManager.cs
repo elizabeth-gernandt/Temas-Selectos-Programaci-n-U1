594b52f050bb50e65880ff3d3d6bb2d70f61083f
elizabeth-gernandt <egernandt157@gmail.com>
2026-04-13 12:18:05 -0600

    listo calixto

---
+++ Assets/Script/AIManager.cs
@@ -0,0 +1,159 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class AIManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player;
+    [SerializeField]
+    public Transform entrance;
+    [SerializeField]
+    public Transform exit;
+
+    public float detectionRange = 2f;
+    public float exitRange = 2f;
+    public float minDistanceFromEntrance = 15f;
+
+    List<NavMeshAgent> agents = new();
+
+    NavMeshTriangulation triangulation;
+    Vector3 entrancePos;
+
+    float detectionRangeSqr;
+    float exitRangeSqr;
+    float minDistanceSqr;
+
+    bool gameWon = false;
+
+    System.Random random = new();
+
+    void Start()
+    {
+        entrancePos =entrance.position;
+        triangulation = NavMesh.CalculateTriangulation();
+
+        detectionRangeSqr = detectionRange*detectionRange;
+        exitRangeSqr = Mathf.Pow(exitRange, 2);
+        minDistanceSqr = Mathf.Pow(minDistanceSqr, 2);
+
+        FindAllEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        bool playerCaught = false;
+
+        foreach (var agent in agents)
+        {
+            if (!agent.enabled) continue;
+
+            if ((agent.transform.position - playerPos).sqrMagnitude < detectionRangeSqr)
+            {
+                playerCaught = true;
+                break;
+            
+            }
+            
+        }
+
+        if (playerCaught) 
+        {
+            TeleportPLayerToEntrance();
+            RelocateALLNPC();
+
+            return;
+        }
+
+        //Jugador en la salida
+
+        if ((playerPos - exit.position).sqrMagnitude < exitRangeSqr)
+        {
+            gameWon = true;
+            Debug.Log("Felicidades Ganaste");
+        }
+
+        //Persecución
+
+        foreach (var agent in agents)
+        {
+            if (agent.enabled && !agent.isStopped)
+            {
+                agent.SetDestination(playerPos);
+            }
+        }
+    }
+
+    //Metodo para llevar a Player a la entrada
+
+    void TeleportPLayerToEntrance()
+    {
+        var cc = player.GetComponent<NavMeshAgent>();
+
+        if (cc != null)
+        {
+            cc.enabled = false;
+        }
+        player.transform.position = entrancePos;
+        if (cc != null)
+        {  
+            cc.enabled = true; 
+        }
+
+        Debug.Log("Player moved to entrance: " + entrancePos);
+    }
+
+    void RelocateALLNPC()
+    {
+        if (triangulation.vertices.Length == 8) return;
+
+        foreach (var agent in agents)
+        {
+            agent.enabled = false;
+            agent.transform.position = GetValidRandomPosition();
+            agent.enabled = true;
+        }
+    }
+
+    Vector3 GetValidRandomPosition()
+    {
+        Vector3 pos;
+
+        do 
+        {
+            int i = random.Next(0,triangulation.indices.Length/3)*3;
+
+            Vector3 v1 = triangulation.vertices[triangulation.indices[i]];
+            Vector3 v2 = triangulation.vertices[triangulation.indices[i+1]];
+            Vector3 v3 = triangulation.vertices[triangulation.indices[i+2]];
+
+            float r1 = (float)random.NextDouble();
+            float r2 = (float)random.NextDouble();
+
+            if (r1 + r2 > 1f)
+            {
+                r1 = 1f - r1;
+                r2 = 1f - r2;
+            }
+
+            pos = v1 + r1 * (v2 - v1) + r2 * (v3 - v1);
+
+        } while ((pos - entrancePos).sqrMagnitude < minDistanceSqr);
+        return pos;
+    }
+
+    void FindAllEnemies()
+    {
+        agents.Clear();
+        foreach (var agent in FindObjectsByType<NavMeshAgent>(FindObjectsSortMode.None))
+        {
+            if (agent.CompareTag("Enemy"))
+            {
+                agents.Add(agent);
+            }
+        }
+    }
+}
